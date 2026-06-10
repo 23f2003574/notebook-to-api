db44bcf01c1a2525f19e5c54df662ece5efbebd2
abhirupdas-iitm <23f2003574@ds.study.iitm.ac.in>
2026-06-10 10:05:59 +0530

    feat: created pipeline runtime execution engine

---
+++ backend/runtime/__init__.py
@@ -0,0 +1,3 @@
+from .pipeline_runtime import (
+    PipelineRuntime
+)
---
+++ backend/runtime/pipeline_runtime.py
@@ -0,0 +1,39 @@
+from typing import Any, Dict
+
+
+class PipelineRuntime:
+
+    def __init__(self):
+
+        self.context: Dict[
+            str,
+            Any
+        ] = {}
+
+    def set_value(
+        self,
+        key: str,
+        value: Any
+    ):
+
+        self.context[key] = value
+
+    def get_value(
+        self,
+        key: str
+    ):
+
+        return self.context.get(
+            key
+        )
+
+    def has_value(
+        self,
+        key: str
+    ):
+
+        return key in self.context
+
+    def clear(self):
+
+        self.context.clear()
